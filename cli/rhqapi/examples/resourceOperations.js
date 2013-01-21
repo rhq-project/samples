4c8dbb515c408ef471e63b4f494a6e8207bd4d85
Libor Zoubek <lzoubek@jezzovo.net>
2013-01-21 15:43:46 +0100

    rhqapi: updated examples & documentation

--- cli/rhqapi/examples/resourceOperations.js
+++ cli/rhqapi/examples/resourceOperations.js
@@ -1,8 +1,8 @@
 /*
 * resourceOperations.js
 * 
-* This example shows how to invoke operations on resource. Operations cannot be scheduled at this time (by specifying future date o f execution),
-* but once invokeOperation finishes you can be almost sure that it actually finished (or timed out)
+* This example shows how to invoke operations on resource. Operations can also be scheduled using scheduleOperation.
+* Once invokeOperation finishes you can be almost sure that it actually finished (or timed out)
 * 
 * @author Libor Zoubek <lzoubek@redhat.com>
 */
@@ -19,3 +19,13 @@ resource.invokeOperation("discovery");
 // run discovery operation with passing parameters
 resource.invokeOperation("discovery",{"detailedDiscovery":True});
 
+// if we are interested in operation result or status
+var result = resource.invokeOperation("viewProcessList")
+println(result["status"])
+println(result["result"])
+println(result["error"])
+
+// schedule operation that will run 10 times each 600 seconds and is going to start right now
+// scheduling operation does not return nothing
+//
+resource.scheduleOperation("discovery",0,600,10,{detailedDiscovery:false})
