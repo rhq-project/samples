49eda428b55cf993ef089b9ac920b3af5f420739
Libor Zoubek <lzoubek@jezzovo.net>
2013-06-17 17:27:52 +0200

    added python sample for scheduling resource operation

--- rest-api/python/README.md
+++ rest-api/python/README.md
@@ -1,11 +1,14 @@
 # Python examples
 
-These require the [httplib2][1] which is not included by default in 
+These require the [httplib2][1] and [requests][2]  which is not included by default in 
 many python installations and needs to be installed separately.
 
 Also required is the JSON module that is standard since Python 2.6
 
-* get_status.rb -- obtain the system status via REST api
+* get_status.py -- obtain the system status via REST api
+* schedule_resource_operation.py -- schedules operation on resource REST api
+* submit_metric_value.py -- sends random metric values for given schedule via REST api
 
 
 [1]: http://bitworking.org/projects/httplib2/ref/module-httplib2.html
+[2]: http://docs.python-requests.org/
---
+++ rest-api/python/schedule_resource_operation.py
@@ -0,0 +1,50 @@
+"""
+Schedules operation on resource and prints it's result.
+
+Looks up platform in inventory, schedules 'discovery' operation and prints it's result.
+
+Requires RHQ 4.8
+"""
+import sys,os,time
+import requests,json
+
+endpoint = 'http://' + os.getenv('RHQ_HOST','localhost') + ':7080/rest/'
+auth = (os.getenv('RHQ_USER','rhqadmin'),os.getenv('RHQ_PASSWORD','rhqadmin'))
+headers = {'accept':'application/json','content-type': 'application/json'}
+
+# lookup a resource
+req = requests.get(endpoint+'resource?category=PLATFORM&ps=1',headers=headers,auth=auth)
+data = req.json()
+if len(data) == 0:
+    raise Exception("Could not lookup platform in inventory")
+resource = data[0]
+
+# lookup operation
+req = requests.get(endpoint+'operation/definitions?resourceId=%d' % resource['resourceId'],headers=headers,auth=auth)
+for opdef in req.json():
+    if opdef['name'] == 'discovery':
+        # now we'll create draft operation
+        print 'Creating draft operation'
+        req = requests.post(endpoint+'operation/definition/%d?resourceId=%d' %
+                (opdef['id'],resource['resourceId']),{},headers=headers,auth=auth)
+        op = req.json()
+        # fix operation body and set operation parameters
+        op['readyToSubmit'] = True
+        del op['links']
+        op['params'] = {'detailedDiscovery':False}
+        # finally we can schedule our operation
+        print 'Scheduling..'
+        req = requests.put(endpoint+'operation/%d' % op['id'],json.dumps(op),headers=headers,auth=auth)
+        history_url = req.json()['links'][0]['history']['href']
+        result = requests.get(history_url,headers=headers,auth=auth)
+        data = result.json()
+        # wait for it
+        while data['status'] == 'In Progress':
+            print 'Waiting for operation..'
+            result = requests.get(history_url,headers=headers,auth=auth)
+            data = result.json()
+            time.sleep(0.3)
+        print 'Operation finished'
+        print 'Status: %s ' % data['status']
+        print 'Result: %s ' % data['result']
+
